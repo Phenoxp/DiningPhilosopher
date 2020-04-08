61cb3401db00e0ca9b072f5b4b2a34087db92718
Achille <guemkus@yahoo.fr>
2020-04-08 06:51:43 -0400

    Concurrency: Solving Philosophers with ReentrantLock

---
+++ src/com/phenoxp/concurrency/Constants.java
@@ -0,0 +1,11 @@
+package com.phenoxp.concurrency;
+
+public class Constants {
+
+    private Constants(){
+    }
+
+    public static final int NUMER_OF_PHILOSOPHERS = 5;
+    public static final int NUMBER_OF_CHOPSTICKS = 5;
+    public static final int SIMULATION_RUNNING_TIME = 5000; // it is in milliseconds
+}
---
+++ src/com/phenoxp/concurrency/State.java
@@ -0,0 +1,5 @@
+package com.phenoxp.concurrency;
+
+public enum State {
+    LEFT, RIGHT;
+}
